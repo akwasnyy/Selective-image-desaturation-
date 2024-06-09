a3ee2f9b9d389c47fb2539bcb297064a4f269ae8
Kamil Milewski <kmilewski@student.agh.edu.pl>
2024-06-10 00:10:05 +0200

    new gui controls at main window

--- newmain.cpp
+++ newmain.cpp
@@ -16,43 +16,33 @@ public:
     void OnOpen(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
-    void OnAdjustImage(wxCommandEvent& event);
+    
 
 private:
     wxDECLARE_EVENT_TABLE();
-};
-
-class ImageAdjustDialog : public wxDialog {
-public:
-    ImageAdjustDialog(wxWindow* parent, wxWindowID id, const wxString& title);
-
-private:
-    wxRadioBox* optionRadioBox;
-    wxRadioBox* channelRadioBox;
-    wxSlider* slider;
     void OnOptionChange(wxCommandEvent& event);
     void OnSliderChange(wxCommandEvent& event);
 
-    wxDECLARE_EVENT_TABLE();
+    wxRadioBox* optionRadioBox;
+    wxRadioBox* channelRadioBox;
+    wxSlider* slider;
 };
+
 
 
 enum {
     ID_Open = 1,
-    ID_AdjustImage = 2
+    
 };
 
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
 EVT_MENU(ID_Open, MyFrame::OnOpen)
 EVT_MENU(wxID_EXIT, MyFrame::OnExit)
 EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
-EVT_MENU(ID_AdjustImage, MyFrame::OnAdjustImage)
+
 wxEND_EVENT_TABLE()
 
-wxBEGIN_EVENT_TABLE(ImageAdjustDialog, wxDialog)
-EVT_RADIOBOX(wxID_ANY, ImageAdjustDialog::OnOptionChange)
-EVT_SLIDER(wxID_ANY, ImageAdjustDialog::OnSliderChange)
-wxEND_EVENT_TABLE()
+
 
 wxIMPLEMENT_APP(MyApp);
 
@@ -69,7 +59,7 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     menuFile->Append(ID_Open, "&Open...\tCtrl-O", "Open an image file");
     menuFile->AppendSeparator();
     menuFile->Append(wxID_EXIT);
-    menuFile->Append(ID_AdjustImage, "&Adjust Image...\tCtrl-A", "Adjust image properties");
+   
     wxMenu* menuHelp = new wxMenu;
     menuHelp->Append(wxID_ABOUT);
 
@@ -78,8 +68,42 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     menuBar->Append(menuHelp, "&Help");
 
     SetMenuBar(menuBar);
+
+    wxPanel* panel = new wxPanel(this, wxID_ANY);
+
+    wxString options[] = { "RGB", "Brightness" };
+    optionRadioBox = new wxRadioBox(panel, wxID_ANY, "Options", wxPoint(20, 20), wxDefaultSize, WXSIZEOF(options), options, 1, wxRA_SPECIFY_COLS);
+    optionRadioBox->Bind(wxEVT_RADIOBOX, &MyFrame::OnOptionChange, this);
+
+    wxString channels[] = { "R", "G", "B" };
+    channelRadioBox = new wxRadioBox(panel, wxID_ANY, "Channels", wxPoint(20, 100), wxDefaultSize, WXSIZEOF(channels), channels, 3, wxRA_SPECIFY_COLS);
+    channelRadioBox->Hide();
+
+    slider = new wxSlider(panel, wxID_ANY, 0, 0, 255, wxPoint(20, 180), wxSize(300, -1), wxSL_HORIZONTAL);
+    slider->Bind(wxEVT_SLIDER, &MyFrame::OnSliderChange, this);
+    slider->Hide();
+
+
     CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
+    SetStatusText("Selective Image Desaturation v0.2");
+}
+
+
+void MyFrame::OnOptionChange(wxCommandEvent& event) {
+    if (optionRadioBox->GetSelection() == 0) { // RGB selected
+        channelRadioBox->Show();
+        slider->Show();
+    }
+    else { // Brightness selected
+        channelRadioBox->Hide();
+        slider->Show();
+    }
+    this->Layout();
+}
+
+void MyFrame::OnSliderChange(wxCommandEvent& event) {
+    int value = slider->GetValue();
+    // Tutaj logika zmiany obrazu
 }
 
 void MyFrame::OnOpen(wxCommandEvent& event) {
@@ -108,48 +132,3 @@ void MyFrame::OnAbout(wxCommandEvent& event) {
     wxMessageBox("Choose a file (.jpg) to open by clicking File->Open or pressing Ctrl+O on your keyboard", "Get some help", wxOK | wxICON_INFORMATION);
 }
 
-ImageAdjustDialog::ImageAdjustDialog(wxWindow* parent, wxWindowID id, const wxString& title)
-    : wxDialog(parent, id, title, wxDefaultPosition, wxSize(250, 300)) {
-    wxString options[] = { "RGB", "Brightness" };
-    optionRadioBox = new wxRadioBox(this, wxID_ANY, "Options", wxDefaultPosition, wxDefaultSize, WXSIZEOF(options), options, 1, wxRA_SPECIFY_COLS);
-
-    wxString channels[] = { "R", "G", "B" };
-    channelRadioBox = new wxRadioBox(this, wxID_ANY, "Channels", wxDefaultPosition, wxDefaultSize, WXSIZEOF(channels), channels, 1, wxRA_SPECIFY_COLS);
-    channelRadioBox->Show(false); // Initially hidden
-
-    slider = new wxSlider(this, wxID_ANY, 0, 0, 255, wxDefaultPosition, wxDefaultSize, wxSL_HORIZONTAL);
-    slider->Show(false); // Initially hidden
-
-    wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-    sizer->Add(optionRadioBox, 0, wxALL, 10);
-    sizer->Add(channelRadioBox, 0, wxALL, 10);
-    sizer->Add(slider, 0, wxALL, 10);
-
-    SetSizer(sizer);
-    Layout();
-}
-
-void ImageAdjustDialog::OnOptionChange(wxCommandEvent& event) {
-    if (optionRadioBox->GetSelection() == 0) { // RGB selected
-        channelRadioBox->Show();
-        slider->Show();
-    }
-    else { // Brightness selected
-        channelRadioBox->Show(false);
-        slider->Show();
-    }
-    Layout();
-}
-
-void ImageAdjustDialog::OnSliderChange(wxCommandEvent& event) {
-    // Tutaj mo¿na zaimplementowaæ logikê zmiany wartoœci obrazu na podstawie pozycji suwaka
-    int value = slider->GetValue();
-    // Zastosuj wartoœæ value do obrazu
-}
-
-void MyFrame::OnAdjustImage(wxCommandEvent& event) {
-    ImageAdjustDialog dialog(this, wxID_ANY, "Adjust Image");
-    if (dialog.ShowModal() == wxID_OK) {
-        // Tutaj mo¿na obs³u¿yæ wyniki z dialogu, jeœli to potrzebne
-    }
-}
