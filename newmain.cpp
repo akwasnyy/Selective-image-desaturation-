7d0c28ff8a0de0267d779dbc411652c6c3f0a10c
K4mi1ewski <milewski.kamil421@gmail.com>
2024-06-10 21:31:42 +0200

    better gui with saving

--- newmain.cpp
+++ newmain.cpp
@@ -15,33 +15,52 @@ class MyFrame : public wxFrame {
 public:
     MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
     void OnOpen(wxCommandEvent& event);
+    void OnSave(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
+    void OnSliderChange(wxCommandEvent& event);
 
     
 
 private:
     wxDECLARE_EVENT_TABLE();
-    void OnOptionChange(wxCommandEvent& event);
-    void OnSliderChange(wxCommandEvent& event);
+    
 
-    wxRadioBox* optionRadioBox;
-    wxRadioBox* channelRadioBox;
-    wxSlider* slider;
+    wxSlider* brightnessSlider;
+    wxSlider* rSlider;
+    wxSlider* gSlider;
+    wxSlider* bSlider;
+
+    cv::Mat original;
+    cv::Mat desaturated;
+
+    wxStaticText* brightness_txt;
+    wxStaticText* r_txt;
+    wxStaticText* g_txt;
+    wxStaticText* b_txt;
 };
 
 
 
 enum {
     ID_Open = 1,
+    ID_Save,
+    ID_Brightness=100,
+    ID_R,
+    ID_G,
+    ID_B
     
 };
 
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
 EVT_MENU(ID_Open, MyFrame::OnOpen)
+EVT_MENU(ID_Save, MyFrame::OnSave)
 EVT_MENU(wxID_EXIT, MyFrame::OnExit)
 EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
-
+EVT_SLIDER(ID_Brightness, MyFrame::OnSliderChange)
+EVT_SLIDER(ID_R, MyFrame::OnSliderChange)
+EVT_SLIDER(ID_G, MyFrame::OnSliderChange)
+EVT_SLIDER(ID_B, MyFrame::OnSliderChange)
 wxEND_EVENT_TABLE()
 
 
@@ -59,6 +78,7 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     : wxFrame(NULL, wxID_ANY, title, pos, size) {
     wxMenu* menuFile = new wxMenu;
     menuFile->Append(ID_Open, "&Open...\tCtrl-O", "Open an image file");
+    menuFile->Append(ID_Save, "&Save...\tCtrl-S", "Save the current image"); // Add Save option
     menuFile->AppendSeparator();
     menuFile->Append(wxID_EXIT);
    
@@ -73,39 +93,47 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
 
     wxPanel* panel = new wxPanel(this, wxID_ANY);
 
-    wxString options[] = { "RGB", "Brightness" };
-    optionRadioBox = new wxRadioBox(panel, wxID_ANY, "Options", wxPoint(20, 20), wxDefaultSize, WXSIZEOF(options), options, 1, wxRA_SPECIFY_COLS);
-    optionRadioBox->Bind(wxEVT_RADIOBOX, &MyFrame::OnOptionChange, this);
+    new wxStaticText(panel, wxID_ANY, "0", wxPoint(20, 45));
+    brightnessSlider = new wxSlider(panel, ID_Brightness, 0, 0, 255, wxPoint(20, 20), wxSize(300, -1), wxSL_HORIZONTAL);
+    new wxStaticText(panel, wxID_ANY, "255", wxPoint(310, 45));
+    //new wxStaticText(panel, wxID_ANY, "Brightness", wxPoint(20, 5));
+    brightness_txt= new wxStaticText(panel, wxID_ANY, "Brightness=0", wxPoint(20,5));
 
-    wxString channels[] = { "R", "G", "B" };
-    channelRadioBox = new wxRadioBox(panel, wxID_ANY, "Channels", wxPoint(20, 100), wxDefaultSize, WXSIZEOF(channels), channels, 3, wxRA_SPECIFY_COLS);
-    channelRadioBox->Hide();
+    new wxStaticText(panel, wxID_ANY, "0", wxPoint(20, 105));
+    rSlider = new wxSlider(panel, ID_R, 0, 0, 255, wxPoint(20, 80), wxSize(300, -1), wxSL_HORIZONTAL);
+    new wxStaticText(panel, wxID_ANY, "255", wxPoint(310, 105));
+    //new wxStaticText(panel, wxID_ANY, "R", wxPoint(20, 65));
+    r_txt = new wxStaticText(panel, wxID_ANY, "R=0", wxPoint(20,65));
 
-    slider = new wxSlider(panel, wxID_ANY, 0, 0, 255, wxPoint(20, 180), wxSize(300, -1), wxSL_HORIZONTAL);
-    slider->Bind(wxEVT_SLIDER, &MyFrame::OnSliderChange, this);
-    slider->Hide();
+    new wxStaticText(panel, wxID_ANY, "0", wxPoint(20, 165));
+    gSlider = new wxSlider(panel, ID_G, 0, 0, 255, wxPoint(20, 140), wxSize(300, -1), wxSL_HORIZONTAL);
+    new wxStaticText(panel, wxID_ANY, "255", wxPoint(310, 165));
+    //new wxStaticText(panel, wxID_ANY, "G", wxPoint(20, 125));
+    g_txt = new wxStaticText(panel, wxID_ANY, "G=0", wxPoint(20,125));
 
+    new wxStaticText(panel, wxID_ANY, "0", wxPoint(20, 225));
+    bSlider = new wxSlider(panel, ID_B, 0, 0, 255, wxPoint(20, 200), wxSize(300, -1), wxSL_HORIZONTAL);
+    new wxStaticText(panel, wxID_ANY, "255", wxPoint(310, 225));
+    //new wxStaticText(panel, wxID_ANY, "B", wxPoint(20, 185));
+    b_txt = new wxStaticText(panel, wxID_ANY, "B=0", wxPoint(20,185));
 
     CreateStatusBar();
     SetStatusText("Selective Image Desaturation v0.2");
 }
 
 
-void MyFrame::OnOptionChange(wxCommandEvent& event) {
-    if (optionRadioBox->GetSelection() == 0) { // RGB selected
-        channelRadioBox->Show();
-        slider->Show();
-    }
-    else { // Brightness selected
-        channelRadioBox->Hide();
-        slider->Show();
-    }
-    this->Layout();
-}
+
 
 void MyFrame::OnSliderChange(wxCommandEvent& event) {
-    int value = slider->GetValue();
-    // Tutaj logika zmiany obrazu
+    int brightnessValue = brightnessSlider->GetValue();
+    int rValue = rSlider->GetValue();
+    int gValue = gSlider->GetValue();
+    int bValue = bSlider->GetValue();
+    //tutaj wrzucic logike
+    brightness_txt->SetLabel(wxString::Format("Brightness=%d", brightnessValue));
+    r_txt->SetLabel(wxString::Format("R=%d", rValue));
+    g_txt->SetLabel(wxString::Format("G=%d", gValue));
+    b_txt->SetLabel(wxString::Format("B=%d", bValue));
 }
 
 void MyFrame::OnOpen(wxCommandEvent& event) {
@@ -113,12 +141,12 @@ void MyFrame::OnOpen(wxCommandEvent& event) {
         "JPEG files (*.jpg)|*.jpg|All files (*.*)|*.*");
 
     if (!filePath.empty()) {
-        cv::Mat image = cv::imread(filePath.ToStdString(), cv::IMREAD_COLOR);
-
+        original = cv::imread(filePath.ToStdString(), cv::IMREAD_COLOR);
+        desaturated = original.clone();
         //zaimplementowac jakis display handler czy cos idk
-        if (!image.empty()) {
-            cv::imshow("Original image", image);  // Pierwsze okno
-            cv::imshow("Desaturated image", image);  // Drugie okno
+        if (!desaturated.empty()) {
+            cv::imshow("Original image", original);  // Pierwsze okno
+            cv::imshow("Desaturated image", desaturated);  // Drugie okno
            // cv::waitKey(0); // Czekaj na naciœniêcie klawisza w dowolnym oknie
             while (true) {
                 cv::waitKey(100);
@@ -141,3 +169,21 @@ void MyFrame::OnAbout(wxCommandEvent& event) {
     wxMessageBox("Choose a file (.jpg) to open by clicking File->Open or pressing Ctrl+O on your keyboard", "Get some help", wxOK | wxICON_INFORMATION);
 }
 
+void MyFrame::OnSave(wxCommandEvent& event) {
+    if (desaturated.empty()) {
+        wxLogError("No image to save.");
+        return;
+    }
+
+    wxString savePath = wxFileSelector("Save image as", "", "", "",
+        "JPEG files (*.jpg)|*.jpg|PNG files (*.png)|*.png|All files (*.*)|*.*", wxFD_SAVE | wxFD_OVERWRITE_PROMPT);
+
+    if (!savePath.empty()) {
+        if (cv::imwrite(savePath.ToStdString(), desaturated)) {
+            wxLogMessage("Image saved successfully.");
+        }
+        else {
+            wxLogError("Failed to save the image.");
+        }
+    }
+}
